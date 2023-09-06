490bb22a6d23d84e80de1bc83bd55b5a0c9a3c99
iamtaufik <muhtopik07@gmail.com>
2023-09-06 21:44:55 +0700

    inisialisasi file index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+class Animal {
+  constructor(name) {
+    this.name = name;
+  }
+
+  eat() {
+    console.log(`${this.name} is eating`);
+  }
+
+  sleep() {
+    console.log(`${this.name} is sleeping`);
+  }
+}
